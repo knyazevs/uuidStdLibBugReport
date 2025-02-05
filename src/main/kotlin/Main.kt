c6c6f398e9a10222d8a2fb2e8a495bd211d038c4
knyazevs <48366704+knyazevs@users.noreply.github.com>
2025-02-05 12:31:23 +0300

    Added Uuid in main function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,10 @@
 package io.github.knyazevs
 
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
+
+@OptIn(ExperimentalUuidApi::class)
 fun main() {
     println("Hello World!")
+    println(Uuid.random().toString())
 }
